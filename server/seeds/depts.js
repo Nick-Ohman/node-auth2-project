ea56d6fccc42a1272c4e923e62ae16ba0550e428
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-06-16 14:11:16 -0500

    database set up, server running

---
+++ server/seeds/depts.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex) {
+
+  return knex('departments').insert([
+    {name: 'customer'},
+    {name: 'sales'},
+    {name: 'marketing'},
+    {name: 'admin'},
+    {name: 'manager'},
+    {name: 'customerserver'}
+  ]);
+};
